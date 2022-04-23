86a8edd9f7383dd4950b6935c9e4cbc1fe7fdce7
Alexander Kozlovich <alexander.kozlovich@gmail.com>
2022-04-23 14:30:48 +0400

    update: disable auto-hide dropdown content

--- src/view/components/GenericDropdown/index.tsx
+++ src/view/components/GenericDropdown/index.tsx
@@ -1,5 +1,4 @@
-import React, { useCallback, useState, FC } from "react";
-import { useTimeout } from "@@view/hooks";
+import React, { useCallback, useEffect, useState, FC, useRef } from "react";
 import * as styles from "./styles";
 
 export interface GenericDropdownProps {
@@ -16,24 +15,30 @@ export interface GenericDropdownRenderContent {
   (hide: () => void): React.ReactNode;
 }
 
-const HIDE_CONTENT_TIMEOUT = 333;
-
 export const GenericDropdown: FC<GenericDropdownProps> = (props) => {
+  const rootRef = useRef<HTMLDivElement>(null);
   const [visible, setVisible] = useState(false);
-  const hideTimeout = useTimeout();
-
-  const handleMouseLeave = useCallback(() => {
-    if (visible) {
-      hideTimeout.start(() => setVisible(false), HIDE_CONTENT_TIMEOUT);
-    }
-  }, [hideTimeout, visible]);
-
-  const handleMouseOver = useCallback(() => hideTimeout.cancel(), [hideTimeout]);
   const handleHide = useCallback(() => setVisible(false), [setVisible]);
   const handleToggle = useCallback(() => setVisible((x) => !x), [setVisible]);
 
+  useEffect(() => {
+    if (!visible) {
+      return;
+    }
+
+    const listener: EventListenerOrEventListenerObject = (event) => {
+      const rootNode = rootRef.current!;
+      const targetNode = event.target as Node;
+      if (document.contains(targetNode) && !rootNode.contains(targetNode)) {
+        setVisible(false);
+      }
+    };
+    document.addEventListener("click", listener);
+    return () => document.removeEventListener("click", listener);
+  }, [visible]);
+
   return (
-    <div css={styles.root} onMouseLeave={handleMouseLeave} onMouseOver={handleMouseOver}>
+    <div ref={rootRef} css={styles.root}>
       {props.renderControl(handleToggle, visible)}
       {visible && (
         <div css={[styles.contentContainer, props.fullWidth && styles.contentContainerStretch]}>
