581ae6c3d9e293af0ce771f81952419ff7968d6a
greatgaetan <gaetan@keiz.ai>
2023-09-28 12:52:06 +0200

    ESEC-43 update material section text

--- src/core/project/utils/formatDocumentSecurityFeatureCategoryString.ts
+++ src/core/project/utils/formatDocumentSecurityFeatureCategoryString.ts
@@ -3,7 +3,7 @@ import { Rpc } from "@@core/rpc/shared";
 export function formatDocumentSecurityFeatureCategoryString(value: Rpc.SFCategory): string {
   switch (value) {
     case Rpc.SFCategory.Material:
-      return "Material";
+      return "Material (data page)";
     case Rpc.SFCategory.Personalization:
       return "Personalization";
     case Rpc.SFCategory.Printed:
